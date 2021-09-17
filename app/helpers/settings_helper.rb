b95b5d2a0bc46d83b2889ae98628a78ccdd71ee5
stsecurity <stsecurity@users.noreply.github.com>
2021-09-17 19:25:06 +0800

    Add interface customisation

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -78,6 +78,7 @@ module SettingsHelper
     vi: 'Tiếng Việt',
     zgh: 'ⵜⴰⵎⴰⵣⵉⵖⵜ',
     'zh-CN': '简体中文',
+    'zh-YR': '简体中文（百合）',
     'zh-HK': '繁體中文（香港）',
     'zh-TW': '繁體中文（臺灣）',
     zh: '中文',
