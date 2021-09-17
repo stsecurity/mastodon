b56ee5d7654e5ecf9896c0dd2ec651da04eb3375
stsecurity <stsecurity@users.noreply.github.com>
2021-09-17 14:59:01 +0800

    Update status_length_validator.rb

--- app/validators/status_length_validator.rb
+++ app/validators/status_length_validator.rb
@@ -2,7 +2,7 @@
 
 class StatusLengthValidator < ActiveModel::Validator
   MAX_CHARS = 3000
-  MAX_UNCUT_CHARS = 500
+  #MAX_UNCUT_CHARS = 500
   URL_PLACEHOLDER_CHARS = 23
   URL_PLACEHOLDER = "\1#{'x' * URL_PLACEHOLDER_CHARS}"
 
@@ -11,7 +11,7 @@ class StatusLengthValidator < ActiveModel::Validator
 
     @status = status
     status.errors.add(:text, I18n.t('statuses.over_character_limit', max: MAX_CHARS)) if too_long?
-    status.errors.add(:text, I18n.t('statuses.over_uncut_character_limit', max: MAX_UNCUT_CHARS)) if too_long_uncut?
+    #status.errors.add(:text, I18n.t('statuses.over_uncut_character_limit', max: MAX_UNCUT_CHARS)) if too_long_uncut?
 
   private
 
@@ -19,21 +19,21 @@ class StatusLengthValidator < ActiveModel::Validator
     countable_length > MAX_CHARS
   end
 
-  def too_long_uncut?
-    (countable_uncut_length > MAX_UNCUT_CHARS) && (countable_spoiler_length < 1)
-  end
+  #def too_long_uncut?
+  #  (countable_uncut_length > MAX_UNCUT_CHARS) && (countable_spoiler_length < 1)
+  #end
 
   def countable_length
     total_text.mb_chars.grapheme_length
   end
 
-  def countable_uncut_length
-    countable_text.mb_chars.grapheme_length
-  end
+  #def countable_uncut_length
+  #  countable_text.mb_chars.grapheme_length
+  #end
 
-  def countable_spoiler_length
-    @status.spoiler_text.mb_chars.grapheme_length
-  end
+  #def countable_spoiler_length
+  #  @status.spoiler_text.mb_chars.grapheme_length
+  #end
 
   def total_text
     [@status.spoiler_text, countable_text].join
